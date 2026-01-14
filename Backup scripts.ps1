ef1343c25e0ba3357b607a6795b7205c91de28d2
AldebaraanMKII <21088681+AldebaraanMKII@users.noreply.github.com>
2026-01-14 05:29:34 -0300

    call functions by command line

--- Backup scripts.ps1
+++ Backup scripts.ps1
@@ -1,3 +1,17 @@
+param (
+    [ValidateSet(
+        "Backup-Character-Main",
+        "Restore-Character-Main",
+        "Backup-All-Accounts-Main",
+        "Restore-All-Accounts-Main",
+        "Backup-Guild-Main",
+        "Restore-Guild-Main",
+        "Backup-All-Guilds-Main-Wrapper",
+        "Restore-All-Guilds-Main"
+    )]
+    [string]$Function
+)
+
 # Install required module if not already installed
 if (-not (Get-Module -ListAvailable -Name SimplySql)) {
     Install-Module -Name SimplySql -Force
@@ -13,59 +27,67 @@ Import-Module PSSQLite
 . "./(Config) Backup scripts.ps1"	# import configuration
 . "./Functions.ps1"	# import functions
 ########################################
+Function Show-Menu {
+	Write-Host "`nWoW Backup/Restore Scripts" -ForegroundColor Green
+	Write-Host "`nSelect a option:" -ForegroundColor Green
+	Write-Host "1. Backup character(s)." -ForegroundColor Green
+	Write-Host "2. Restore character(s)." -ForegroundColor Green
+	Write-Host "3. Backup all accounts and characters." -ForegroundColor Green
+	Write-Host "4. Restore all accounts and characters." -ForegroundColor Green
+	Write-Host "5. Backup guild(s)." -ForegroundColor Green
+	Write-Host "6. Restore guild(s)." -ForegroundColor Green
+	Write-Host "7. Backup all guilds." -ForegroundColor Green
+	Write-Host "8. Restore all guilds." -ForegroundColor Green
+	Write-Host "9. Exit script" -ForegroundColor Green
+}
+
+
 try {
 	# Start logging
 	$CurrentDate = Get-Date -Format "yyyyMMdd_HHmmss"
 	Start-Transcript -Path "./logs/BackupScripts_$($CurrentDate).log" -Append
 	
-	$exitScript = $false
-	while (-not $exitScript) {
-		Write-Host "`nWoW Backup Scripts" -ForegroundColor Green
-		Write-Host "`nSelect a option:" -ForegroundColor Green
-		Write-Host "1. Backup character(s)." -ForegroundColor Green
-		Write-Host "2. Restore character(s)." -ForegroundColor Green
-        Write-Host "3. Backup all accounts and characters." -ForegroundColor Green
-        Write-Host "4. Restore all accounts and characters." -ForegroundColor Green
-		Write-Host "5. Backup guild(s)." -ForegroundColor Green
-		Write-Host "6. Restore guild(s)." -ForegroundColor Green
-        Write-Host "7. Backup all guilds." -ForegroundColor Green
-        Write-Host "8. Restore all guilds." -ForegroundColor Green
-		Write-Host "9. Exit script" -ForegroundColor Green
-		
-		$choice = $(Write-Host "`nType a number (1-9):" -ForegroundColor green -NoNewLine; Read-Host) 
-########################################
-		if ($choice -eq 1) {
-			Backup-Character-Main
-########################################
-		} elseif ($choice -eq 2){
-			Restore-Character-Main
-########################################
-        } elseif ($choice -eq 3){
-            Backup-All-Accounts-Main
-########################################
-        } elseif ($choice -eq 4){
-            Restore-All-Accounts-Main
-################################################################################
-		} elseif ($choice -eq 5){
-			Backup-Guild-Main
-########################################
-		} elseif ($choice -eq 6){
-			Restore-Guild-Main
-########################################
-        } elseif ($choice -eq 7){
-            Backup-All-Guilds-Main-Wrapper
-########################################
-        } elseif ($choice -eq 8){
-            Restore-All-Guilds-Main
-######################################## exit
-		} elseif ($choice -eq 9){
-			exit
-########################################
-		} else {
-			Write-Host "`nInvalid choice. Try again." -ForegroundColor Red
-		}
-########################################
-	}
+    if ($Function) {
+        Invoke-Expression -Command $Function
+    } else {
+        $exitScript = $false
+        while (-not $exitScript) {
+            Show-Menu #shows menu options
+            $choice = $(Write-Host "`nType a number (1-9):" -ForegroundColor green -NoNewLine; Read-Host) 
+    ########################################
+            if ($choice -eq 1) {
+                Backup-Character-Main
+    ########################################
+            } elseif ($choice -eq 2){
+                Restore-Character-Main
+    ########################################
+            } elseif ($choice -eq 3){
+                Backup-All-Accounts-Main
+    ########################################
+            } elseif ($choice -eq 4){
+                Restore-All-Accounts-Main
+    ################################################################################
+            } elseif ($choice -eq 5){
+                Backup-Guild-Main
+    ########################################
+            } elseif ($choice -eq 6){
+                Restore-Guild-Main
+    ########################################
+            } elseif ($choice -eq 7){
+                Backup-All-Guilds-Main-Wrapper
+    ########################################
+            } elseif ($choice -eq 8){
+                Restore-All-Guilds-Main
+    ######################################## exit
+            } elseif ($choice -eq 9){
+                exit
+    ########################################
+            } else {
+                Write-Host "`nInvalid choice. Try again." -ForegroundColor Red
+            }
+    ########################################
+        }
+    }
 ########################################
 } catch {
 	Write-Host "An error occurred: $($_.Exception.Message)" -ForegroundColor Red
